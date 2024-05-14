2a7211c0a1923efba800183c0708c671c2f906d7
Bohdan Yablinchuk 44987 <yablinchuk9@gmail.com>
2024-05-14 17:54:47 +0200

    final commit

---
+++ routes/trainings.js
@@ -0,0 +1,13 @@
+//routes/trainings.js
+const express = require('express');
+const router = express.Router();
+const trainingController = require('../controllers/trainingController');
+
+router.get('/', trainingController.training_list);
+router.get('/new', trainingController.training_create_get);
+router.post('/', trainingController.training_create_post);
+router.get('/:id/edit', trainingController.training_update_get);
+router.put('/:id', trainingController.training_update_put);
+router.delete('/:id', trainingController.training_delete);
+
+module.exports = router;
